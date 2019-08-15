1c317b73640a26429a8458edc0d7f1235a3e2feb
C3NZ <cenz@cenz.io>
2019-08-14 18:26:44 -0700

    [add] eslint and some of the functions

--- index.js
+++ index.js
@@ -1,5 +1,86 @@
-export class D {
+export default class Time {
   constructor(...args) {
     this.date = new Date(...args);
+    this.monthMap = {
+      0: 'January',
+      1: 'February',
+      2: 'March',
+      3: 'April',
+      4: 'May',
+      5: 'June',
+      6: 'July',
+      7: 'August',
+      8: 'September',
+      9: 'October',
+      10: 'November',
+      11: 'December',
+    };
+  }
+
+  /**
+   * @function
+   * @desc Get the full year from the date
+   * @return {Number} The full year
+   * @example
+   * Time().getFullYear() -> 2019
+   */
+  year() {
+    return this.date.getFullYear();
+  }
+
+  /**
+   * @function
+   * @desc Get the month from the current date
+   * @return {Number} Get the full month
+   * @example
+   * Time('10/10/2019').getFullYear() -> October
+   */
+  month() {
+    const month = this.monthMap[this.date.getMonth()];
+    return month;
+  }
+
+  /**
+   * @function
+   * @desc Get the day of the month
+   * @return {Number} The full year
+   * @example
+   * Time().getFullYear() -> 2019
+   */
+  day() {
+    return this.date.getUTCDate();
+  }
+
+  /**
+   * @function
+   * @desc Get the full year from the date
+   * @return {Number} The full year
+   * @example
+   * Time().getFullYear() -> 2019
+   */
+  hours() {
+    return this.date.getHours();
+  }
+
+  /**
+   * @function
+   * @desc Get the full year from the date
+   * @return {Number} The full year
+   * @example
+   * Time().getFullYear() -> 2019
+   */
+  minutes() {
+    return this.date.getMinutes();
+  }
+
+  /**
+   * @function
+   * @desc Get the full year from the date
+   * @return {Number} The full year
+   * @example
+   * Time().getFullYear() -> 2019
+   */
+  seconds() {
+    return this.date.getSeconds();
   }
 }
