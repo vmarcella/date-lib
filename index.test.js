80462c41dfaf737984eb7efbe5731f0d87bc6132
C3NZ <cenz@cenz.io>
2019-08-18 19:46:44 -0700

    [add] tests for the month functionality

--- index.test.js
+++ index.test.js
@@ -7,3 +7,12 @@ test('The date library returns the correct year', () => {
   date = new Time('12/12/2020');
   expect(date.year()).toBe(2020);
 });
+
+test('The date library returns the correct month', () => {
+  let date = new Time('09/19/1997');
+  expect(date.month()).toBe('September');
+  date = new Time('12/12/2020');
+  expect(date.month()).toBe('December');
+  date = new Time('10/30/2067');
+  expect(date.month()).toBe('October');
+});
